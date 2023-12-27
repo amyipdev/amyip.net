787f44c92f5fc9c35bb03f920b7e867b0e941f62
Amy Parker <amy@amyip.net>
2023-12-26 21:47:34 -0800

    cat: No such file or directory
    This patch adds the NSFOD error to cat.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- svelte/wasm/src/unix/cat.rs
+++ svelte/wasm/src/unix/cat.rs
@@ -14,8 +14,13 @@ pub fn cat(term: &Terminal, mut args: Vec<&str>) -> i32 {
     let (opts, files) = args.split_at_mut(split);
     // TODO: actually respect cat options
     for f in files {
+		let txt = crate::vfs::futils::read_to_end(f.to_string());
+		if txt.is_none() {
+			term.writeln(&format!("cat: {}: No such file or directory", f));
+			return -2;
+		}
         term.writeln(
-            &String::from_utf8(crate::vfs::futils::read_to_end(f.to_string()).unwrap()).unwrap(),
+            &String::from_utf8(txt.unwrap()).unwrap(),
         );
     }
     return 0;
