07b0c179fa02e97afeb610d9b7d0bbfdea5f5cd0
Amy Parker <amy@amyip.net>
2024-02-10 17:07:10 -0800

    navbar improvements
    This patch improves the navbar.
    
    Signed-off-by: Amy Parker <amy@amyip.net>

--- svelte/src/Home.svelte
+++ svelte/src/Home.svelte
@@ -1,72 +1,8 @@
 <script lang="ts">
-    import {
-        Navbar,
-        Collapse,
-        NavbarBrand,
-        NavItem,
-        NavbarToggler,
-        NavLink,
-        Nav
-    } from "sveltestrap";
-    let isOpen = false;
-    function hu(e: CustomEvent) {
-        isOpen = e.detail.isOpen;
-    }
-    import {sw} from './stores';
-    window.onscroll = () => {};
+	import Nbar from "./Nbar.svelte";
 </script>
 
 <main>
-    <!-- TODO: move this navbar to its own component -->
-    <Navbar expand="md">
-        <NavbarBrand><span id="navbar-brand">amyip.net</span></NavbarBrand>
-        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-        <Collapse {isOpen} navbar expand="md" on:update={hu}>
-            <Nav class="ms-auto" navbar>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        about
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink on:click={() => sw.set(2)}>
-                        shell
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        cv
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        blog
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        run
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        projects
-                    </NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink disabled on:click={() => {}}>
-                        contact
-                    </NavLink>
-                </NavItem>
-            </Nav>
-        </Collapse>
-    </Navbar>
-    
+	<Nbar active="about"></Nbar>
 </main>
 
-<style>
-    #navbar-brand {
-        font-weight: 700;
-        color: #f6f2e6 !important;
-    }
-</style>
---
+++ svelte/src/Nbar.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+    import {
+		Navbar,
+		Collapse,
+		NavbarBrand,
+		NavItem,
+		NavbarToggler,
+		NavLink,
+		Nav
+	} from "sveltestrap";
+    let isOpen = false;
+    function hu(e: CustomEvent) {
+		isOpen = e.detail.isOpen;
+	}
+    import {sw} from './stores';
+    window.onscroll = () => {};
+    export let active: string;
+</script>
+
+<main>
+    <!-- TODO: move this navbar to its own component -->
+    <Navbar expand="md">
+        <NavbarBrand><span id="navbar-brand">amyip.net</span></NavbarBrand>
+        <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+        <Collapse {isOpen} navbar expand="md" on:update={hu}>
+            <Nav class="ms-auto" navbar>
+                <NavItem>
+                    <NavLink active={active == "about"} on:click={() => sw.set(1)}>
+                        about
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink on:click={() => sw.set(2)}>
+                        shell
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={active == "cv"} disabled on:click={() => {}}>
+                        cv
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={active == "blog"} disabled on:click={() => {}}>
+                        blog
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={active == "run"} disabled on:click={() => {}}>
+                        run
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={active == "projects"} on:click={() => {}}>
+                        projects
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink active={active == "contact"} on:click={() => {}}>
+                        contact
+                    </NavLink>
+                </NavItem>
+            </Nav>
+        </Collapse>
+    </Navbar>
+
+</main>
+
+<style>
+    #navbar-brand {
+        font-weight: 700;
+        color: #f6f2e6 !important;
+    }
+</style>
